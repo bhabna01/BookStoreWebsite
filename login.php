7f1e8314540a9af32f620b9132330434453ff7e0
Abier Farzana <101545745+bhabna01@users.noreply.github.com>
2022-03-15 12:06:38 +0600

    Update login.php

--- login.php
+++ login.php
@@ -49,7 +49,7 @@ if(isset($_POST['submit'])){
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-   <!-- custom css file link  -->
+
    <link rel="stylesheet" href="css/style.css">
 
 </head>
