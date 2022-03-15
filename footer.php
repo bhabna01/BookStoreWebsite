dee08655f88ac61ddb6b69a9c10a409f69786842
Nadia Sultana <101514077+nadiasultana08@users.noreply.github.com>
2022-03-15 12:33:28 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
 
       <div class="box">
          <h3>contact info</h3>
-         <p> <i class="fas fa-phone"></i> 01714983802 </p>
+         <p> <i class="fas fa-phone"></i> 019758965416 </p>
          <p> <i class="fas fa-phone"></i> 019727085264 </p>
          <p> <i class="fas fa-envelope"></i> group14@gmail.com </p>
          <p> <i class="fas fa-map-marker-alt"></i> Jahangirnagar University, Savar, Dhaka </p>
